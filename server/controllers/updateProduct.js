15478f35fd2b9e9c39b53c2c01ad62aa3154226d
Tusar36 <tusar.w07@gmail.com>
2023-11-12 15:19:17 +0530

    Server changes

---
+++ server/controllers/updateProduct.js
@@ -0,0 +1,29 @@
+const product = require("../models/ProductModel");
+
+const updateProduct = async (req, res) => {
+  const id = req.params.id;
+  const findProduct = await product.findById(id);
+  if (!findProduct) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
+
+  try {
+    await product.updateOne(
+      { _id: id },
+      {
+        $set:req.body,
+      }
+    );
+    res.status(200).json({
+        message:"Product update"
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+module.exports = updateProduct;
--- server/routes/ProductRouter.js
+++ server/routes/ProductRouter.js
@@ -6,10 +6,12 @@ const getAllProduct = require("../controllers/getAllProduct");
 const getProductDetails = require("../controllers/getProductDetails");
 const createProduct = require("../controllers/createProduct");
 const addReview = require("../controllers/addReview");
+const updateProduct = require("../controllers/updateProduct");
 
 router.get("/all", getAllProduct);
 router.get("/get/:id", getProductDetails);
 router.post("/create", createProduct);
 router.post("/addReview/:id", addReview);
+router.post("/update/:id", updateProduct);
 
 module.exports = router;
