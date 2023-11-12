bf570e90b2d23ad96adf994e5c13508a179a47f2
Tusar36 <tusar.w07@gmail.com>
2023-11-12 16:09:51 +0530

    Delete product route created

---
+++ server/controllers/deleteProduct.js
@@ -0,0 +1,17 @@
+const product = require('../models/ProductModel')
+
+const deleteProduct = async (req,res) =>{
+    const id = req.params.id
+    try {
+        await product.deleteOne({_id:id})
+        res.status(200).json({
+            message:"Product Deleted"
+        })
+    } catch (error) {
+        res.status(500).json({
+            error
+        })
+    }
+}
+
+module.exports = deleteProduct
--- server/routes/ProductRouter.js
+++ server/routes/ProductRouter.js
@@ -7,11 +7,13 @@ const getProductDetails = require("../controllers/getProductDetails");
 const createProduct = require("../controllers/createProduct");
 const addReview = require("../controllers/addReview");
 const updateProduct = require("../controllers/updateProduct");
+const deleteProduct = require("../controllers/deleteProduct");
 
 router.get("/all", getAllProduct);
 router.get("/get/:id", getProductDetails);
 router.post("/create", createProduct);
 router.post("/addReview/:id", addReview);
-router.post("/update/:id", updateProduct);
+router.put("/update/:id", updateProduct);
+router.delete("/delete/:id", deleteProduct);
 
 module.exports = router;
